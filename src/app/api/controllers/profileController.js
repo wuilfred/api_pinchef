d0a236dae1f7c430614db2ba5a8f36851e9445e8
Osmel Smith Ruiz <osmelsr97@gmail.com>
2020-11-24 00:02:40 -0500

    CRUD profile (partial)

--- src/app/api/controllers/profileController.js
+++ src/app/api/controllers/profileController.js
@@ -3,7 +3,6 @@ const Role = require('../../../helpers/role');
 const profileModel = require('../models/profileModel');
 const Joi = require('joi');
 
-
 module.exports = {
     create,
     detail,
@@ -12,22 +11,24 @@ module.exports = {
 };
 
 async function create(req, res, next) {
-
     const id_user = req.params.id;
-    const user = req.body;
-    const validator = await validatorProfile(user);
-
-    if (!validator.isValid) {
-        return res.status(400).json({
-            status: false,
-            message: validator.message,
-        });
-    }
-
+    const { profile, address, chef } = req.body;
     try {
+        // Step 1 - Save profile
+        await validatorProfile(profile);
         const conn = await pool.getConnection();
-        const result = await conn.query(profileModel.Create(id_user, user));
+        const result = await conn.query(profileModel.Create(id_user, profile));
         conn.release();
+
+        if (result.affectedRows > 0) {
+            const id_profile = result.insertId;
+            // If it's chef save him
+            if (profile.role === Role.Chef) {
+                const create_Chef = await createChef(chef, id_profile)
+            }
+            // Step 3 - Save address
+            const create_address = await createAddress(id_user, id_profile, address, profile.address);
+        }
 
         res.status(200).json({
             status: true,
@@ -36,20 +37,17 @@ async function create(req, res, next) {
         });
 
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         return res.status(500).json({
             status: false,
             message: 'Operation failed',
-            data: null
+            details: error.message
         });
     }
-
 }
 
 async function detail(req, res, next) {
-
     const id_user = req.params.id;
-
     try {
         const conn = await pool.getConnection();
         const result = await conn.query(profileModel.Detail(id_user));
@@ -65,28 +63,19 @@ async function detail(req, res, next) {
         return res.status(500).json({
             status: false,
             message: 'Operation failed',
-            data: null
+            details: error.message
         });
     }
-
 }
 
 async function update(req, res, next) {
-
     const id_user = req.params.id;
     const user = req.body;
-    const validator = await validatorProfile(user);
-
-    if (!validator.isValid) {
-        return res.status(400).json({
-            status: false,
-            message: validator.message,
-        });
-    }
-
     try {
+        await validatorProfile(user);
         const conn = await pool.getConnection();
         const result = await conn.query(profileModel.Update(id_user, user));
+        const id_profile = result.insertId;
         conn.release();
 
         res.status(200).json({
@@ -96,19 +85,16 @@ async function update(req, res, next) {
         });
 
     } catch (error) {
-        console.log('Update', error);
         return res.status(500).json({
             status: false,
             message: 'Operation failed',
-            data: null
+            details: error.message
         });
     }
 }
 
 async function _delete(req, res, next) {
-
     const id_user = req.params.id;
-
     try {
         const conn = await pool.getConnection();
         const result = await conn.query(profileModel.Delete(id_user));
@@ -124,40 +110,90 @@ async function _delete(req, res, next) {
         return res.status(500).json({
             status: false,
             message: 'Operation failed',
-            data: null
+            details: error.message
         });
     }
-    
 }
 
-async function validatorProfile(user) {
-
+async function createAddress(user_id_user, profile_id_profile, address, first_address) {
     const schema = Joi.object({
-        name: Joi.string()
-            .required(),
-        lastname: Joi.string()
-            .required(),
-        birthday: Joi.date()
-            .required(),
-        gender: Joi.string()
-            .required(),
-        phone: Joi.number()
-            .required(),
+        first_address: Joi.string().required(),
+        second_address: Joi.string().required(),
+        country: Joi.string().required(),
+        state_region: Joi.string().required(),
+        city: Joi.string().required(),
+        postcode: Joi.number().required(),
+        lat_lon: Joi.string().required(),
+        about_info: Joi.string().required(),
+        profile_id_profile: Joi.required(),
+        user_id_user: Joi.required()
+    });
+
+    const addressData = {
+        ...address,
+        first_address,
+        profile_id_profile,
+        user_id_user
+    };
+
+    try {
+        const value = await schema.validateAsync(addressData);
+
+    } catch (err) {
+        throw new Error(err);
+    }
+
+    const conn = await pool.getConnection();
+    const result = await conn.query(profileModel.CreateAddress(addressData));
+    conn.release();
+}
+
+async function createChef(chef, profile_id_profile) {
+    const schema = Joi.object({
+        short_intro: Joi.string().required(),
+        long_intro: Joi.string().required(),
+        services_name: Joi.string().required(),
+        service_availability: Joi.string().required(),
+        price: Joi.number().required(),
+        position: Joi.string().required(),
+        languages: Joi.string().required(),
+        address: Joi.string().required(),
+        location_service: Joi.string().required(),
+        banner: Joi.string().required(),
+        profile_id_profile: Joi.required()
+    });
+
+    const chefData = { ...chef, profile_id_profile };
+
+    try {
+        const value = await schema.validateAsync(chefData);
+
+    } catch (error) {
+        throw new Error(error);
+    }
+
+    const conn = await pool.getConnection();
+    const result = await conn.query(profileModel.CreateChef(chefData));
+    conn.release();
+}
+
+async function validatorProfile(profile) {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        lastname: Joi.string().required(),
+        birthday: Joi.date().required(),
+        gender: Joi.string().required(),
+        phone: Joi.number().required(),
         status: Joi.number(),
-        photo: Joi.string()
-            .required(),
-        address: Joi.string()
-            .required(),
-        role: Joi.any()
-            .valid(Role.Chef, Role.User)
-            .required()
+        photo: Joi.string().required(),
+        role: Joi.any().valid(Role.Chef, Role.User).required(),
+        address: Joi.string().required()
     });
 
     try {
-        const value = await schema.validateAsync(user);
-        return { isValid: true };
-    }
-    catch (err) {
-        return { isValid: false, message: err.message };
+        const value = await schema.validateAsync(profile);
+
+    } catch (err) {
+        throw new Error(err);
     }
 }
--- src/app/api/models/profileModel.js
+++ src/app/api/models/profileModel.js
@@ -5,7 +5,6 @@ class Profile {
     }
 
     Create(id_user, profile) {
-
         this.rs = `INSERT INTO profile (name, lastname, birthday, gender, phone, status, photo, address, role, user_id_user)
                   VALUES ('${profile.name}', '${profile.lastname}', '${profile.birthday}', '${profile.gender}', '${profile.phone}',
                   1,' ${profile.photo}', '${profile.address}', '${profile.role}', '${id_user}' )`;
@@ -13,14 +12,32 @@ class Profile {
         return this.rs;
     }
 
-    Detail(id_user) {
+    CreateAddress(address) {
+        this.rs = `INSERT INTO address_book (country, first_address, second_address, state_region, city, postcode, lat_lon,
+                  about_info, profile_id_profile, user_id_user, created, updated)
+                  VALUES ('${address.country}', '${address.first_address}', '${address.second_address}', '${address.state_region}',
+                 '${address.city}', '${address.postcode}',' ${address.lat_lon}', '${address.about_info}', '${address.profile_id_profile}',
+                 '${address.user_id_user}', now(), now())`;
 
+        return this.rs;
+    }
+
+    CreateChef(chef) {
+        this.rs = `INSERT INTO chef (short_intro, long_intro, services_name, service_availability, price, position, languages,
+                  address, location_service, banner, profile_id_profile)
+                  VALUES ('${chef.short_intro}', '${chef.long_intro}', '${chef.services_name}', '${chef.service_availability}',
+                 '${chef.price}', '${chef.position}',' ${chef.languages}', '${chef.address}', '${chef.location_service}',
+                 '${chef.banner}', '${chef.profile_id_profile}')`;
+
+        return this.rs;
+    }
+
+    Detail(id_user) {
         this.rs = `SELECT * FROM profile WHERE user_id_user = ${id_user}`;
         return this.rs;
     }
 
     Update(id_user, profile) {
-
         this.rs = `UPDATE profile SET name = '${profile.name}', lastname = '${profile.lastname}', birthday = '${profile.birthday}',
                   gender = '${profile.gender}', phone = '${profile.phone}', status = '${profile.status}', photo = '${profile.photo}',
                   address = '${profile.address}', role = '${profile.role}'
