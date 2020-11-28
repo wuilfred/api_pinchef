c4ea0504a7b74b4fbc8a5134d2bc7df14bbd27dc
HeikelV <heikelvillar@gmail.com>
2020-11-27 23:23:54 -0500

    api doc

---
+++ doc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "API PC",
+  "version": "0.0.1",
+  "description": "API for PC Management",
+  "title": "API PC",
+  "url": "http://localhost:8001/api",
+  "sampleUrl": "http://localhost:8001/api",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-11-28T04:22:25.906Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
